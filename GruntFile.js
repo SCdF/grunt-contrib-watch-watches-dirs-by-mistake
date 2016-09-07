46b8a6f239ee5672071f8477306b164162022385
Stefan du Fresne <stefan@sdufresne.info>
2016-09-07 14:46:24 +0100

    Initial commit

---
+++ GruntFile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    exec: {
+      printstuff: {
+        cmd: 'echo "printstuff has been fired!"'
+      }
+    },
+    watch: {
+      shouldFire: {
+        files: 'shouldFire',
+        tasks: ['printstuff']
+      }
+    }
+  });
+
+  grunt.registerTask('printstuff', 'Prints out some debug', [
+    'exec:printstuff',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', '', [
+    'watch'
+  ]);
+};
